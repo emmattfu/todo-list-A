32feca275f40bccd5c5af6ec755cab08786491ad
emmattfu <makcimdjan@gmail.com>
2018-08-31 19:47:38 +0300

    enitial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/todo-list')));
+
+app.get('*', (request, res) => {
+  res.sendFile(path.join(__dirname, 'dist/todo-list/index.html'))
+});
+
+const port = process.env.PORT || '3000';
+
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
