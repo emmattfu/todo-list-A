e054ef26861fe5ffcaf4c1b71253935c866050c1
emmattfu <makcimdjan@gmail.com>
2018-09-01 14:08:57 +0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
 import { RoutingModule } from "./routing.module";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
-import { NewTodoComponent } from "./components/new-todo/new-todo.component";
 import { HttpClientModule } from "@angular/common/http";
-import { TodoEditComponent } from './components/todo-edit/todo-edit.component';
 import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from "@angular/forms";
-import { RegisterComponent } from './components/register/register.component';
-import { MyDatePipe } from "./pipes/my-date.pipe";
-import { SumPipe } from "./pipes/sum.pipe";
-import { MyNgStyleDirective } from './directives/my-ng-style.directive';
-import { ClassDirective } from './directives/class.directive';
 
 import { HomeModule } from "./modules/home-module/home.module";
-import {AuthModuleModule} from "./modules/auth-module/auth-module.module";
+import { AuthModuleModule } from "./modules/auth-module/auth-module.module";
 
 @NgModule({
   declarations: [
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { NewTodoComponent } from './components/new-todo/new-todo.component';
-import { TodoEditComponent } from "./components/todo-edit/todo-edit.component";
-// import { LoginComponent } from "./components/login/login.component";
 import { AuthGuard } from "./guards/auth.guard";
-import { RegisterComponent } from "./components/register/register.component";
 
 import { homeRoutes } from "./modules/home-module/home-routing";
 import { authRoutes } from "./modules/auth-module/auth-routing";
@@ -14,10 +9,7 @@ import { authRoutes } from "./modules/auth-module/auth-routing";
 
 const routes: Routes = [
   { path: 'login', children: [...authRoutes] },
-  // { path: 'register', children: [...authRoutes]},
   { path: '', children: [...homeRoutes], canActivate: [AuthGuard] },
-  // { path: 'add-todo', component: NewTodoComponent, canActivate: [AuthGuard] },
-  // { path: 'todos/:id', component: TodoEditComponent, canActivate: [AuthGuard]},
   { path: '**', component: NotFoundComponent }
 ];
 
